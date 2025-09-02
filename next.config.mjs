f8fad91402753730a3fe5b3dd62d7062c79dc442
Ayush Baranwal <abaran803@gmail.com>
2025-09-02 09:27:33 +0530

    Add Netlify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
-export default nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+};
+export default nextConfig;
